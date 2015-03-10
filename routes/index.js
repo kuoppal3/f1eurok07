be36382a235b3009f04101485de23ae6d0873050
kuoppal3 <kuoppala@gmail.com>
2015-03-10 20:47:48 +0000

    azuretest

--- routes/index.js
+++ routes/index.js
@@ -41,9 +41,9 @@ exports.index = function(req, res) {
   var request = require('request');
   // http://f1-eurok07-kuoppal3.c9.io
   // http://f1eurok07.azurewebsites.net
-  request.get('http://f1eurok07.azurewebsites.net/files/nimet_test.dat', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var csv = body;
+ // request.get('http://f1-eurok07-kuoppal3.c9.io/files/nimet_test.dat', function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  /*    var csv = body;
       var names = csv.split('\n');
 
       for(var i = 0; i < names.length; ++i) {
@@ -64,15 +64,15 @@ exports.index = function(req, res) {
         // Skip to the next player
         i = i + 5;
 
-      }
+      }*/
       
       // Count totalranks
       for(var i = 0; i < drivers.length; ++i) {
         var driverName = drivers[i].name;
         var driverPoints = drivers[i].points;
         
-        console.log("nimi: " + driverName);
-        console.log("pisteet: " + driverPoints);
+        //console.log("nimi: " + driverName);
+        //console.log("pisteet: " + driverPoints);
         
         // Go through all the players and count their rank
         for(var p = 0; p < players.length; ++p) {
@@ -80,12 +80,12 @@ exports.index = function(req, res) {
           // Go through all the players' drivers and give rankpoints each of them
           for(var dr = 0; dr < players[p].driverRanks.length; ++dr) {
             if(dr === i && players[p].driverRanks[dr].name === driverName) {
-              console.log("Osu");
+              //console.log("Osu");
               players[p].driverRanks[dr].color = "green";
               players[p].totalRank += parseFloat(driverPoints);
             // Hit but not exact hit
             } else if(players[p].driverRanks[dr].name === driverName) {
-              console.log("listassa");
+              //console.log("listassa");
               players[p].driverRanks[dr].color = "yellow";
               players[p].totalRank += parseFloat(driverPoints) * 0.1;
             }
@@ -94,12 +94,12 @@ exports.index = function(req, res) {
       }
       
       // TODO: lasttime ranks
-      console.log(players[0].driverRanks);
+      //console.log(players[0].driverRanks);
       // Copy the array
       var sortedPlayers = players.slice(0);
       sortedPlayers.sort(function(a, b) { return b.totalRank - a.totalRank});
       res.render('index', { players: players, sortedPlayers: sortedPlayers, drivers: drivers });
-    }
-  });
+  //  }
+  //});
   
 };

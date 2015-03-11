da2f6e2397bdd4c14e4799beaf8bc8b332292950
kuoppal3 <kuoppala@gmail.com>
2015-03-11 15:35:10 +0000

    navbar changes

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ exports.index = function(req, res) {
   var request = require('request');
   // http://f1-eurok07-kuoppal3.c9.io
   // http://f1eurok07.azurewebsites.net
-  request.get('http://f1-eurok07-kuoppal3.c9.io/files/nimet_test.txt', function (error, response, body) {
+  request.get('http://f1eurok07.azurewebsites.net/files/nimet_2014.txt', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var csv = body;
       csv = csv.replace("Ã¤", 'ä').replace("Ã¶", 'ö');
